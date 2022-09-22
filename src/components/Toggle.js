55820b0c0d33782de746d49eabca243929c6d3ba
imbarilyn <linahmuhonjaimbari@gmail.com>
2022-09-22 15:54:06 +0300

    background color addd to the button

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Toggle() {
-  return <button>OFF</button>;
+  const [isOn, setState] = useState(false)
+  function handleClick() {
+    setState((isOn) => !isOn)
+  }
+
+  const color = isOn? "red": "white";  
+  
+
+  return <button style = {{background: color}} onClick={handleClick}>{isOn? "ON": "OFF"}</button>
 }
 
 export default Toggle;
